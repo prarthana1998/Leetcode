e2f812f412cab8b1b5004edcc569454019dcf010
Prarthana Kandwal <41475054+prarthana1998@users.noreply.github.com>
2024-09-05 15:21:03 +0100

    Time: 108 ms (67.55%), Space: 18.3 MB (65.31%) - LeetHub

--- 1094-matrix-cells-in-distance-order/1094-matrix-cells-in-distance-order.py
+++ 1094-matrix-cells-in-distance-order/1094-matrix-cells-in-distance-order.py
@@ -1,13 +1,11 @@
 class Solution:
     def allCellsDistOrder(self, row: int, col: int, rCenter: int, cCenter: int):
-        result = []
-        for r in range(row):
-            for c in range(col):
-                # Append each cell's coordinates
-                result.append([r, c])
+       
+       result = []
 
-        # Sort the result list by Manhattan distance to the center
-        # Manhattan distance is |r - rCenter| + |c - cCenter|
-        result.sort(key=lambda x: abs(x[0] - rCenter) + abs(x[1] - cCenter))
+       for r in range(row):
+        for c in range(col):
+            result.append((r,c))
 
-        return result
+       result.sort(key=lambda x: abs(rCenter - x[0]) + abs(cCenter - x[1]))
+       return result
