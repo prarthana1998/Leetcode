6a555383e91ff972a18dd694e9e49658d2e1c314
Prarthana Kandwal <41475054+prarthana1998@users.noreply.github.com>
2024-09-10 10:46:53 +0100

    Time: 122 ms (87.06%), Space: 10 MB (89.69%) - LeetHub

--- 2332-count-lattice-points-inside-a-circle/2332-count-lattice-points-inside-a-circle.cpp
+++ 2332-count-lattice-points-inside-a-circle/2332-count-lattice-points-inside-a-circle.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
-    int countLatticePoints(vector<vector<int>>& circles) {
-
-        set<pair<int, int>> unique_pair;
-
-        for(auto &circle : circles)
-        {
-            int a = circle[0];
-            int b = circle[1];
-            int r = circle[2];
-
-            int r_squared = r*r;
-        
-        for(int x = -r; x <= r; x++)
-        {
-            for(int y = -r; y<=r ; y++)
-            {
-                if((x*x)+(y*y) <= r_squared)
-                unique_pair.insert({a+x, b+y});
+    int countLatticePoints(vector<vector<int>>& v) {
+  int n = v.size();
+        int ans = 0;
+        for(int x=0;x<=200;x++){
+            for(int y=0;y<=200;y++){
+                for(int i=0;i<n;i++){
+                    int a=v[i][0],b=v[i][1],r=v[i][2];
+                    if((a-x)*(a-x) + (b-y)*(b-y) <= r*r){
+                        ans++;
+                        break;
+                    }
+                }
             }
         }
+            return ans;
         }
-        return unique_pair.size();
-    }
+    
 };
