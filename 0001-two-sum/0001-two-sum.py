caecaa066867b9547c282457d970aeb499604236
Prarthana Kandwal <41475054+prarthana1998@users.noreply.github.com>
2024-09-16 13:42:35 +0100

    Time: 59 ms (64.35%), Space: 17.7 MB (30.18%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -7,12 +7,10 @@ class Solution:
         n = len(nums)
 
         for i in range(n):
-            numMap[nums[i]] = i
-
-        for i in range(n):
             complement = target - nums[i]
-            if complement in numMap and numMap[complement]!=i:
+            if complement in numMap:
                 return [i, numMap[complement]]
+            numMap[nums[i]] = i
 
         return []
 
