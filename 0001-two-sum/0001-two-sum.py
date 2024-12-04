e283b8b6b95a0a81d3eea6d1b53e910d3fc48bf8
Prarthana Kandwal <41475054+prarthana1998@users.noreply.github.com>
2024-12-04 16:25:38 +0000

    Time: 0 ms (100%), Space: 18.6 MB (5.38%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,13 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
 
-        numsMap={}
-        n = len(nums)
-
-        for i in range(n):
-            key = target - nums[i]
-            if(key in numsMap):
-                return [i, numsMap[key]]
-            numsMap[nums[i]]=i
-            
-        return[]
+       numsMap={}
+       n = len(nums)
+       for i in range(n):
+        key = target - nums[i]
+        if key in numsMap:
+            return[numsMap[key], i]
+        numsMap[nums[i]] = i
+       return[]
+    
