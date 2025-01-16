f5a0f9d31518a786f862c53612a2b8508fe18d93
Prarthana Kandwal <41475054+prarthana1998@users.noreply.github.com>
2025-01-16 11:31:14 +0000

    Time: 9 ms (78.81%), Space: 17.7 MB (60.18%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,11 +1,13 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         numsMap = defaultdict(int)
-        for x in s:
-            numsMap[x]+=1
-        for x in t:
-            numsMap[x]-=1
+        for strs in s:
+            numsMap[strs]+=1
+        for strs in t:
+            numsMap[strs]-=1
+        
         for val in numsMap.values():
             if val!=0:
                 return False
         return True
+        
