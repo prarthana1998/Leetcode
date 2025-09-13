79b4211fdcce2239251866fde264ea5919555903
Prarthana Kandwal <41475054+prarthana1998@users.noreply.github.com>
2025-09-13 16:49:22 +0100

    Time: 3 ms (50.21%), Space: 18.8 MB (61.92%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,12 +1,21 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        numsMap = {}
-        for num in range(len(nums)):
-            # if target-number present return indices else add
-            
-            res = target-nums[num]
-            if res in numsMap:
-                return [numsMap[res],num]
-            numsMap[nums[num]] = num
+        '''
+        dict => key: numbers, value: index
+        traverse the dict
+        check
+        1) if target - key exists in the dict
+        s1: present return the indices 
+        s2: present return add it to the dict
+        2, 7, 11, 15
+        0  1.  2.  3 target = 9 
+        dict = {2:0, }
+        '''
+
+        number_dict = {}
+        for i, num in enumerate(nums):
+            if target-nums[i] in number_dict:
+                return [number_dict[target-nums[i]], i]
+            number_dict[nums[i]] = i
         return {}
-        
+ 
