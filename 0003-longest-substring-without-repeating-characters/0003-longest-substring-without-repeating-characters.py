263e69058b4fddc746a9a2845c2b10a680957b3b
Prarthana Kandwal <41475054+prarthana1998@users.noreply.github.com>
2026-05-08 11:15:59 +0100

    Time: 11 ms (77.12%), Space: 19.2 MB (69.34%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -3,12 +3,11 @@ class Solution:
         hashSet = set()
         max_length = 0
         left = 0
-
         for right in range(len(s)):
-            while s[right] in hashSet:
-                    hashSet.remove(s[left])
-                    left+=1
-               
+            while(s[right] in hashSet):
+                hashSet.remove(s[left])
+                left += 1
+                
             hashSet.add(s[right])
             max_length = max(max_length, right - left + 1)
 
