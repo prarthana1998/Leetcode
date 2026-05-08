cbf963bfdb78cc78fce28142c486e667c1e1a3ce
Prarthana Kandwal <41475054+prarthana1998@users.noreply.github.com>
2026-05-08 10:48:37 +0100

    Time: 0 ms (100%), Space: 19.4 MB (27.3%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,16 +1,24 @@
 class Solution:
+    '''
+    classifying ques -> empty string?
+    hash map = close key open value
+    |
+    |
+    |
+    '''
     def isValid(self, s: str) -> bool:
         stack = []
-        Hashmap = {")": "(", "]": "[","}": "{"}
+        hashmap = { ")" : "(", "]" : "[", "}" : "{" }
 
-        for i in s:
-            if i in Hashmap:
-                if stack and stack[-1] == Hashmap[i]:
+        for c in s:
+            if c in hashmap:
+                if stack and stack[-1] == hashmap[c]:
                     stack.pop()
                 else:
                     return False
-            
             else:
-                stack.append(i)
-
+                stack.append(c)
+        
         return True if not stack else False
+       
+
